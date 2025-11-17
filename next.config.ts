38f6326963d7ef7baf56e2d13bbad57c39531fb5
designwiz <33524911+designwiz@users.noreply.github.com>
2025-11-17 09:31:32 +0000

    Working locally - Twilwind + PWA Fixes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  // No experimental React Compiler or other flags for now
 };
 
 export default nextConfig;
